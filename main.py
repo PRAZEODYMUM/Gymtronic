9879244f60270e15bb1ec47dd52ee9275ee73ead
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 11:07:23 +0100

    main.py: major update

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from data import *
 from core import get_workout
+
 n=input('Чем сегодня займёмся? ')
 choice=(input('Сколько упражнений? '))
+
+choice_dict={
+'back':exercices_back,
+'cardio':exercices_cardio,
+'chest':exercices_chest,
+'core':exercices_core,
+'legs':exercices_legs,
+'shoulders':exercices_shoulders
+}
+
+if n.lower() not in choice_dict:
+    print('Неверный ввод, правильные вводы:')
+    for n in choice_dict.keys():
+        print(n)
+    quit()
+
 try:
     choice_int=int(choice)
 except ValueError:
     print(f'Неверный ввод количества')
-    quit()
-choice_dict={
-'back':get_workout(exercices_back,choice_int),
-'cardio':get_workout(exercices_cardio,choice_int),
-'chest':get_workout(exercices_chest,choice_int),
-'core':get_workout(exercices_core,choice_int),
-'legs':get_workout(exercices_legs,choice_int),
-'shoulders':get_workout(exercices_shoulders,choice_int)
-}
-if n.lower() in choice_dict:
-    print(choice_dict[n.lower()])
-else:
-    keys=choice_dict.keys()
-    print('Неверный ввод упражнения, правильные вводы: ')
-    for n in keys:
-        print(n)
-    quit()
+    quit()    
+
+print(get_workout(choice_dict[n.lower()],choice_int))

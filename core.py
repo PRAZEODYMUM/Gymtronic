5d0d00a806fdfb766eee2ddb5996d675e5ca7b63
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 11:18:28 +0100

    core.py: minor update
    changed exercices to exercises

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 import random
-def get_workout(exercices:dict,count):
-    if len(exercices)<count:
-        print(f'Недостаточно упражнений в конфиге, беру {len(exercices)}')
-        return random.sample(sorted(exercices.values()),len(exercices))
-    return random.sample(sorted(exercices.values()),count)
+def get_workout(exercises:dict,count):
+    if len(exercises)<count:
+        print(f'Недостаточно упражнений в конфиге, беру {len(exercises)}')
+        return random.sample(sorted(exercises.values()),len(exercises))
+    return random.sample(sorted(exercises.values()),count)

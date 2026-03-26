d6a217f464006afc36f1289f254f1e4aef024bee
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 18:45:58 +0100

    main.py: update

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from core import all_exercises,get_workout
+from core import all_exercises,get_workout,difficulties
+import random
 #-
-n=input('What\'s ond your mind today? ')
+n=input('What\'s on your mind today? ').lower()
 #-
-if n.lower() not in all_exercises:
+if n not in all_exercises:
     print('Invalid input, valid inputs:')
     for key in all_exercises.keys():
         print(key)
     quit()
+#-
+diff=input('Difficulty? ').lower()
+#-
+if diff not in difficulties:
+    raise Exception('Invalid input')
 #-
 choice=(input('How many exercises? '))    
 #-
@@ -21,4 +27,11 @@ if choice_int<=0:
 if choice_int>len(all_exercises[n]):
     raise Exception('Insuficient exercise ammount in config')      
 #-
-print(get_workout(all_exercises[n.lower()],choice_int))
+workout=get_workout(all_exercises[n],choice_int)
+print(f'\nYour {n} workout ({len(workout)} exercises):')
+print('-'*30)
+for i,exercise in enumerate(workout,1):
+    sets=random.randint(*difficulties[diff]['sets'])
+    reps=random.randint(*difficulties[diff]['reps'])
+    print(f' {i}.{exercise} - {sets} sets and {reps} reps')
+print('-'*30)

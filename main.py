cf4ab44758380d92e78173f8c359ba112bc856d4
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 11:24:10 +0100

    main.py: update
    added choice_int verification,
    added dividers for code readability

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from data import *
 from core import get_workout
-
+#-
 n=input('Чем сегодня займёмся? ')
-
+#-
 choice_dict={
 'back':exercises_back,
 'cardio':exercises_cardio,
@@ -11,19 +11,23 @@ choice_dict={
 'legs':exercises_legs,
 'shoulders':exercises_shoulders
 }
-
+#-
 if n.lower() not in choice_dict:
     print('Неверный ввод, правильные вводы:')
     for n in choice_dict.keys():
         print(n)
     quit()
-
+#-
 choice=(input('Сколько упражнений? '))
-
+#-
 try:
     choice_int=int(choice)
 except ValueError:
     print(f'Неверный ввод количества')
-    quit()    
-
+    quit()
+#-
+if choice_int<=0:
+    print('Неверный ввод количества')
+    quit()
+#-
 print(get_workout(choice_dict[n.lower()],choice_int))

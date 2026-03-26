679c6ffacd011fac8099648daeb7b49c15f6f9fc
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 11:49:40 +0100

    main.py: major update

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-from data import *
-from core import get_workout
+import json
+from core import all_exercises,get_workout
 #-
 n=input('Чем сегодня займёмся? ')
 #-
-choice_dict={
-'back':exercises_back,
-'cardio':exercises_cardio,
-'chest':exercises_chest,
-'core':exercises_core,
-'legs':exercises_legs,
-'shoulders':exercises_shoulders
-}
-#-
-if n.lower() not in choice_dict:
+if n.lower() not in all_exercises:
     print('Неверный ввод, правильные вводы:')
-    for n in choice_dict.keys():
+    for n in all_exercises.keys():
         print(n)
     quit()
 #-
-choice=(input('Сколько упражнений? '))
+choice=(input('Сколько упражнений? '))    
 #-
 try:
     choice_int=int(choice)
@@ -28,6 +19,6 @@ except ValueError:
 #-
 if choice_int<=0:
     print('Неверный ввод количества')
-    quit()
+    quit()    
 #-
-print(get_workout(choice_dict[n.lower()],choice_int))
+print(get_workout(all_exercises[n.lower()],choice_int))

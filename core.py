611a2e63a842bd5413346cb3d79815d9e71543ef
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 11:50:30 +0100

    core.py: major update

--- core.py
+++ core.py
@@ -1,7 +1,15 @@
+import json
+import os
 import random
+#-
+base_dir=os.path.dirname(os.path.abspath(__file__))
+data_path=os.path.join(base_dir,'data.json')
+#-
+with open(data_path,'r',encoding='utf-8') as f:
+    all_exercises=json.load(f)
 #-
 def get_workout(exercises:dict,count):
     if len(exercises)<count:
         print(f'Недостаточно упражнений в конфиге, беру {len(exercises)}')
-        return random.sample(sorted(exercises.values()),len(exercises))
-    return random.sample(sorted(exercises.values()),count)
+        return random.sample(list(exercises.values()),len(exercises))
+    return random.sample(list(exercises.values()),count)

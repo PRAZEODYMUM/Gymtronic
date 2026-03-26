e979c3c1f6c6ab7347aa1be3246f9923d62e07dc
PRAZEODYMUM <antontkachov.wns@gmail.com>
2026-03-26 18:45:34 +0100

    core.py: update

--- core.py
+++ core.py
@@ -6,7 +6,9 @@ base_dir=os.path.dirname(os.path.abspath(__file__))
 data_path=os.path.join(base_dir,'data.json')
 #-
 with open(data_path,'r',encoding='utf-8') as f:
-    all_exercises=json.load(f)
+    data=json.load(f)
+    all_exercises={k: v for k, v in data.items() if k!='difficulties'}
+    difficulties=data['difficulties']
 #-
 def get_workout(exercises:dict,count):
     if len(exercises)<count:
